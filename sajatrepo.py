5bdf6795fe826d1bc5da8a6bd7b169eedd764229
denesmor <denesmorgotli@gmail.com>
2023-12-12 08:34:31 +0100

    3 feladat

--- sajatrepo.py
+++ sajatrepo.py
@@ -5,9 +5,13 @@ c = random.randint(0,10)
 a = int(input("adj meg egy szamot amit négyzetre szeretnél emelni: "))
 b = a**2
 
+print(b)
+
 if c==b:
     print("szerencséd van")
 elif c>=b:
     print("alá tippeltél")
 else:
     print("fölé tippeltél")
+
+
